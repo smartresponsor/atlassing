4ec945eaae00cdabdd845c448ddc9ef7a99635af
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-24 17:22:08 -0500

    load atlassing bundle configuration

--- src/DependencyInjection/AtlassingExtension.php
+++ src/DependencyInjection/AtlassingExtension.php
@@ -21,12 +21,18 @@ final class AtlassingExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $atlas = is_array($configs[0]['atlas'] ?? null) ? $configs[0]['atlas'] : [];
+
         $container->setParameter('atlassing.component', 'atlassing');
         $container->setParameter('atlassing.package', 'atlassing/atlas');
-        $container->setParameter('atlassing.database_prefix', 'atlas_');
+        $container->setParameter('atlassing.database_prefix', $atlas['database_prefix'] ?? 'atlas_');
+        $container->setParameter('atlassing.documentating_index_path', $atlas['documentating_index_path'] ?? '%kernel.project_dir%/var/documentating/article-index.json');
+        $container->setParameter('atlassing.import_path', $atlas['import_path'] ?? '%kernel.project_dir%/import/documentating-export');
+        $container->setParameter('atlassing.surface_owner_root', $atlas['surface_owner_root'] ?? 'atlassing');
+        $container->setParameter('atlassing.standalone', $atlas['standalone'] ?? true);
         $container->setParameter('atlassing.project_dir', '%kernel.project_dir%');
-        $container->setParameter('atlassing.atlas_root', '%kernel.project_dir%/var/atlas');
-        $container->setParameter('atlassing.python_engine_dir', '%kernel.project_dir%/tools/atlas/python-engine');
+        $container->setParameter('atlassing.atlas_root', $atlas['atlas_root'] ?? '%kernel.project_dir%/var/atlas');
+        $container->setParameter('atlassing.python_engine_dir', $atlas['python_engine_dir'] ?? '%kernel.project_dir%/tools/atlas/python-engine');
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
